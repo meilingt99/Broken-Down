b16b93144d81de51f2d8940f3a36773180b172bd
Meiling Thompson <meilinglthompson@gmail.com>
2019-07-27 17:48:53 -0400

    Added table view of 3 categories of to do tasks, added night mode

--- Broken Down/ViewController.swift
+++ Broken Down/ViewController.swift
@@ -8,8 +8,93 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You selected row \(indexPath.row) in section \(indexPath.section)")
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3;
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return dailyTasks.count
+        case 1:
+            return weeklyTasks.count
+        case 2:
+            return toDos.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        switch indexPath.section {
+        case 0:
+            cell.textLabel?.text = dailyTasks[indexPath.row]
+        case 1:
+            cell.textLabel?.text = weeklyTasks[indexPath.row]
+        case 2:
+            cell.textLabel?.text = toDos[indexPath.row]
+        default:
+            cell.textLabel?.text = "This should never happen"
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Daily Tasks"
+        case 1:
+            return "Weekly Tasks"
+        case 2:
+            return "To Dos"
+        default:
+            return nil
+        }
+    }
+    
+    
+    
+    let dailyTasks = ["Complete 1 CtCI Problem",
+                      "Complete 1 HR/LC Problem",
+                      "Daily Workout",
+                      "Work on CBE for 20 mins"]
+    
+    let weeklyTasks = ["Do Laundry",
+                       "Clean Room",
+                       "Check Finances"]
+    
+    let toDos = ["W2W: Get Map on Homepage",
+                 "W2W: Add SS Page",
+                 "iOS Part 5"]
 
+    @IBAction func nightMode(_ sender: Any) {
+        if view.backgroundColor == UIColor.darkGray {
+            view.backgroundColor = UIColor.white
+            let everything = view.subviews
+            for eachView in everything {
+                if eachView is UILabel {
+                    let currentLabel = eachView as! UILabel
+                    currentLabel.textColor = UIColor.black
+                }
+            }
+        }
+        else {
+            view.backgroundColor = UIColor.darkGray
+            let everything = view.subviews
+            for eachView in everything {
+                if eachView is UILabel {
+                    let currentLabel = eachView as! UILabel
+                    currentLabel.textColor = UIColor.lightGray
+                }
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
